a989ec5cbed934bd7dda09b79b9d77c1635b6da9
KimDayeong <dyyukim@gmail.com>
2024-04-13 22:20:49 +0900

    change weather icon

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,7 @@
 const API_KEY = config.apiKey;
 
-const weather = document.querySelector("#weather span");
-const icon = document.querySelector("#weather img");
+const weather = document.querySelector("#weather span:first-child");
+const icon = document.querySelector("#weather span:last-child");
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 
@@ -10,14 +10,35 @@ function onGeoOk(position) {
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
+  //console.log(url);
+
   fetch(url)
     .then((resopnse) => resopnse.json())
     .then((data) => {
       weather.innerText = `${data.name}/${data.main.temp}°`;
 
-      const weatherIcon = data.weather[0].icon;
-      const iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-      icon.src = iconUrl;
+      const weatherIcon = data.weather[0].description;
+      const i = document.createElement("i");
+
+      if (weatherIcon === "clear sky") {
+        i.setAttribute("class", "fa-solid fa-sun");
+      } else if (
+        weatherIcon === "few clouds" ||
+        weatherIcon === "scattered clouds" ||
+        weatherIcon === "broken clouds"
+      ) {
+        i.setAttribute("class", "fa-solid fa-cloud");
+      } else if (weatherIcon === "shower rain" || weatherIcon === "rain") {
+        i.setAttribute("class", "fa-solid fa-cloud-rain");
+      } else if (weatherIcon === "thunderstorm") {
+        i.setAttribute("class", "fa-solid fa-cloud-bolt");
+      } else if (weatherIcon === "snow") {
+        i.setAttribute("class", "fa-solid fa-snowman");
+      } else if (weatherIcon === "mist") {
+        i.setAttribute("class", "fa-solid fa-bars-staggered");
+      }
+
+      icon.append(i);
     });
 }
 
